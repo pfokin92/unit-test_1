b9564700227f0527da96ced28ebbf06c024026cb
Pavel Fokin <fokinpavel2007@outlook.com>
2021-04-02 08:53:09 +0300

    first_commit

---
+++ src/js/test/app.test.js
@@ -0,0 +1,31 @@
+﻿import getHealth from '../app';
+
+test('health status 1', ()=>{
+    let result = getHealth({ name: 'Маг', health: 85 });
+    
+    expect(result).toBe('healthy');
+});
+
+test('health status 2', ()=>{
+    let result = getHealth({ name: 'Маг', health: 50 });
+    
+    expect(result).toBe('healthy');
+});
+
+test('health status 3', ()=>{
+    let result = getHealth({ name: 'Маг', health: 25 });
+    
+    expect(result).toBe('wounded');
+});
+
+test('health status 4', ()=>{
+    let result = getHealth({ name: 'Маг', health: 15 });
+    
+    expect(result).toBe('wounded');
+});
+
+test('health status 5', ()=>{
+    let result = getHealth({ name: 'Маг', health: 10 });
+    
+    expect(result).toBe('critical');
+});
