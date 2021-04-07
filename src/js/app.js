892cd65410b0ce68c97f6a68416d213ca37bfd44
Pavel Fokin <fokinpavel2007@outlook.com>
2021-04-07 20:58:45 +0300

    Add eslint check

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
-﻿export default function getHealth(obj) {
-    let result;
-    if(obj.health >= 50){
-        result = 'healthy';
-    } else if (obj.health<50 && obj.health >=15) {
-        result = 'wounded';
-    } else if (obj.health < 15){
-        result = 'critical';
-    }
-    return result;
+export default function getHealth(obj) {
+  let result;
+  if (obj.health >= 50) {
+    result = 'healthy';
+  } else if (obj.health < 50 && obj.health >= 15) {
+    result = 'wounded';
+  } else if (obj.health < 15) {
+    result = 'critical';
+  }
+  return result;
 }
--- src/js/test/app.test.js
+++ src/js/test/app.test.js
@@ -1,31 +1,26 @@
-﻿import getHealth from '../app';
+import getHealth from '../app';
 
-test('health status 1', ()=>{
-    let result = getHealth({ name: 'Маг', health: 85 });
-    
-    expect(result).toBe('healthy');
+test('health status 1', () => {
+  const result = getHealth({ name: 'Маг', health: 85 });
+  expect(result).toBe('healthy');
 });
 
-test('health status 2', ()=>{
-    let result = getHealth({ name: 'Маг', health: 50 });
-    
-    expect(result).toBe('healthy');
+test('health status 2', () => {
+  const result = getHealth({ name: 'Маг', health: 50 });
+  expect(result).toBe('healthy');
 });
 
-test('health status 3', ()=>{
-    let result = getHealth({ name: 'Маг', health: 25 });
-    
-    expect(result).toBe('wounded');
+test('health status 3', () => {
+  const result = getHealth({ name: 'Маг', health: 25 });
+  expect(result).toBe('wounded');
 });
 
-test('health status 4', ()=>{
-    let result = getHealth({ name: 'Маг', health: 15 });
-    
-    expect(result).toBe('wounded');
+test('health status 4', () => {
+  const result = getHealth({ name: 'Маг', health: 15 });
+  expect(result).toBe('wounded');
 });
 
-test('health status 5', ()=>{
-    let result = getHealth({ name: 'Маг', health: 10 });
-    
-    expect(result).toBe('critical');
+test('health status 5', () => {
+  const result = getHealth({ name: 'Маг', health: 10 });
+  expect(result).toBe('critical');
 });
